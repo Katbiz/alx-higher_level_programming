530058f68cc49cb37ce8e19f6133fcc3284787ba
Katbiz <ramothamok77@gmail.com>
2023-08-16 05:09:12 -0700

    The if else, loops, and functions project

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-lastdigit = abs(number)%10
-
-print_message = ("Last digit of" + " " + number + " is " + lastdigit)
-
+lastdigit = abs(number) % 10
+if number < 0:
+    lastdigit = -lastdigit
+print("Last digit of {} is {} and is ".format(number, lastdigit), end="")
 if lastdigit > 5:
-    print(print_message + " and is greater than 5")
-
+    print("greater than 5")
+    
 elif lastdigit == 0:
-    print(print_message + " and is 0")
-
+    print("0")
+    
 else:
-    print(print_message + " and is less than 6 and not 0\n")
+    print("less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 10-add.py
+
+ """This funtion returns the addition of a and b."""
+def add(a, b):
+    return (a + b)
