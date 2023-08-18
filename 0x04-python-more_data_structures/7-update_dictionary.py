77cf17682143703e7f1416ce64c18e84a931464b
Katbiz <ramothamok77@gmail.com>
2023-08-18 11:39:09 -0700

    more data structures on python

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+ """This function replaces or add key/value pairs in a dictionary."""
+
+def update_dictionary(a_dictionary, key, value):
+    
+    a_dictionary[key] = value
+    return (a_dictionary)
