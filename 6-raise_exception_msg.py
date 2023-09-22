f0374fc5974feb100ea21a715f22a8a2f3fa05cb
Katbiz <125537174+Katbiz@users.noreply.github.com>
2023-09-22 07:33:54 +0200

    Delete 6-raise_exception_msg.py

--- 6-raise_exception_msg.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def raise_exception_msg(message=""):
-    """A function that raises a name exception with a message.
-
-    Args:
-        message: string of message to be raised
-    """
-    raise NameError(message)
