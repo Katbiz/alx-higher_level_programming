24afdb3d44fc89d1d9e920df326d3f53d1e35e03
Katbiz <125537174+Katbiz@users.noreply.github.com>
2023-09-22 07:22:09 +0200

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
- """This function replaces or add key/value pairs in a dictionary."""
-
-def update_dictionary(a_dictionary, key, value):
-    
-    a_dictionary[key] = value
-    return (a_dictionary)
