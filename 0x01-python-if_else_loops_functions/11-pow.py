b39b9718f19c985912a23622b17996b834b43cac
Katbiz <125537174+Katbiz@users.noreply.github.com>
2023-09-22 07:21:16 +0200

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/11-pow.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# 11-pow.py
-
- """Return a to the power of b."""
-def pow(a, b):
-    return (a ** b)
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for letters in range(97, 123):
-    if chr(letters) is not 'q' and chr(letters) is not 'e':
-        print("{}".format(chr(letter)), end="")
