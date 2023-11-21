6b9c6046f3f348bd79537873d65e3f2584321dca
Katbiz <ramothamok77@gmail.com>
2023-11-20 19:11:17 -0800

    the raise funion task

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """function that raises a type exception.
+    This is done delibrately in order to avoid using the try block.
+    You are not allowed to import any module.
+    """
+    raise TypeError
