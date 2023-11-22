2685ed04394972828e130c5caa4b0f559ba907d8
Katbiz <ramothamok77@gmail.com>
2023-11-21 16:38:14 -0800

    second commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    """
+    def __init__(self, size):
+        """Creates new instances of square (1 side).
+
+        """
+        self.__size = size
